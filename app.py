6da4a71a8f102425e3f141362058ddd7584fecec
Alif Amarta <alifamarta23@gmail.com>
2025-06-27 00:02:22 +0700

    Update: revamp code

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ x_train = scaler.fit_transform(x_train)
 x_test = scaler.fit_transform(x_test)
 
 # streamlit 
-st.title('Prediksi Diagnosis ME/CFS dan Depression Dengan Metode k-NN ')
-k = st.slider('Pilih value dari K untuk k-NN', 1, 15, 5)
+st.title('Prediksi Diagnosa ME/CFS dan Depression Dengan Metode k-NN ')
+k = st.slider('Pilih value dari k untuk k-NN', 1, 15, 5)
 
 # train
 knn = KNeighborsClassifier(n_neighbors=k)
@@ -39,19 +39,59 @@ knn.fit(x_train, y_train)
 y_pred = knn.predict(x_test)
 
 # predict input from user
-st.subheader('Prediksi Diagnosis')
+st.subheader('Prediksi Diagnosa')
 input_data = {}
+
 
 for col in df.columns:
     if col == 'diagnosis':
         continue
 
-    if x[col].dtype == 'int64' or x[col].dtype == 'float64':
-        input_data[col] = st.number_input(col, value=float(x[col].median()))
-    
+    if col == 'age':
+        input_data[col] = st.slider('Umur', 18, 100, 40)
+
     elif col == 'gender':
-        input_data[col] = st.selectbox(col, options=['Male', 'Female'])
-        
+        input_data[col] = st.selectbox('Jenis Kelamin', ['Male', 'Female'])
+
+    elif col == 'sleep_quality_index':
+        input_data[col] = st.slider('Sleep Quality Index (0-10)', 0.0, 10.0, 5.0)
+
+    elif col == 'brain_fog_level':
+        input_data[col] = st.slider('Level Brain Fog (0-10)', 0.0, 10.0, 5.0)
+
+    elif col == 'physical_pain_score':
+        input_data[col] = st.slider('Skor Kesakitan Fisik (0-10)', 0.0, 10.0, 5.0)
+
+    elif col == 'stress_level':
+        input_data[col] = st.slider('Level Stress (0-10)', 0.0, 10.0, 5.0)
+
+    elif col == 'depression_phq9_score':
+        input_data[col] = st.slider('Skor Depresi PHQ-9 (0-27)', 0, 27, 10)
+
+    elif col == 'fatigue_severity_scale':
+        input_data[col] = st.slider('Skor Skala Kelelahan (0-10)', 0.0, 0.10, 5.0)
+    
+    elif col == 'pem_duration_hours':
+        input_data[col] = st.slider('Durasi PEM (jam)', 0.0, 48.0, 24.0)
+    
+    # elif col == 'hours_of_sleep_per_night':
+
+
+    # elif col == 'pem_present':
+
+
+    # elif col == 'work_status':
+
+    
+    # elif col == 'social_activity_level':
+
+
+    # elif col == 'exercise_frequency':
+
+    
+    # elif col == 'meditation_or_mindfulness':
+
+
     else:
         input_data[col] = st.text_input(col, value=str(x[col].mode()[0]))
 
@@ -68,4 +108,4 @@ if st.button('Prediksi'):
     input_df = scaler.transform(input_df)
     pred = knn.predict(input_df)
 
-    st.success(f'Hasil Prediksi Diagnosis: {pred[0]}')
+    st.success(f'Hasil Prediksi Diagnosa: {pred[0]}')
